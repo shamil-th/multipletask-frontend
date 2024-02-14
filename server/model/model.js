127f3a063236bf5b4ecc2e6d1ab42bbc137522a3
shamil-th <shamil.stackup@gmail.com>
2024-02-14 16:31:22 +0530

    test

---
+++ server/model/model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const taskListSchema = new mongoose.Schema({
+  list: [
+    {
+      title: { type: String },
+      type: { type: String },
+      task: { type: mongoose.Schema.Types.Mixed },
+      completed: { type: Boolean },
+    },
+  ],
+  taskTitle:{
+    type:String
+  },
+  pin: { type: Number },
+}, { timestamps: true });
+
+const taskList = new mongoose.model('task', taskListSchema);
+module.exports = taskList
